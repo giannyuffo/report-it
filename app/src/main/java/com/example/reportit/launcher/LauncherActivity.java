cfa821e5c6e91d4512ec2276e5ae1f2db23106d7
Gianny Uffo <uffogianny@gmail.com>
2022-11-29 12:46:00 +0100

    ChatUserActivity created. This will permit a fast acces to a chat session. QuestionnaireActivity will start asking questions to recopilate info about the problem thats happening

--- app/src/main/java/com/example/reportit/launcher/LauncherActivity.java
+++ app/src/main/java/com/example/reportit/launcher/LauncherActivity.java
@@ -6,11 +6,19 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 
 import com.example.reportit.R;
+import com.example.reportit.screens.login.auth.login.LoginActivity;
+import com.example.reportit.screens.login.main.chat.ChatUserActivity;
+import com.example.reportit.screens.login.main.ui.questionaire.QuestionnaireActivity;
 
 public class LauncherActivity extends AppCompatActivity {
 
@@ -18,21 +26,29 @@ public class LauncherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launcher);
-
-        //SharedPreferences sharedPreferences = getSharedPreferences("user");
+        SharedPreferences sharedPreferences = getSharedPreferences("user", Context.MODE_PRIVATE);
         Intent intent;
-        ActivityResultLauncher<String[]> locationPermissionRequest =
-                registerForActivityResult(new ActivityResultContracts
-                        .RequestMultiplePermissions(), result ->{
-                    Boolean fineLocationGranted = result.getOrDefault(
-                            Manifest.permission.ACCESS_FINE_LOCATION, false);
-
-                });
-
+        if(sharedPreferences.getString("token", null) != null && sharedPreferences.getString("", null)!= null){
+            if(sharedPreferences.getString("", null).contains(""))
+                intent = new Intent(this, ChatUserActivity.class);
+            else
+                intent = new Intent(this, QuestionnaireActivity.class );
+        }else
+            intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
     private String getLocation(){
 
         return null;
+    }
+    //Oculta el teclado cuando se clicka en cualquier parte de la Activity.
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (getCurrentFocus() != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(this.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
---
+++ app/src/main/java/com/example/reportit/screens/login/main/ui/questionaire/QuestionnaireActivity.java
@@ -0,0 +1,16 @@
+package com.example.reportit.screens.login.main.ui.questionaire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.reportit.R;
+
+public class QuestionnaireActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionnaire);
+    }
+}
